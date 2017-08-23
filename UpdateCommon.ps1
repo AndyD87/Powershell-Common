d3ea23cc613b0c76aaa99125271f1578f85b8e44
Andreas <coolcow_ccos@yahoo.com>
2017-08-23 17:45:42 +0200

    App download in separate Script

--- UpdateCommon.ps1
+++ UpdateCommon.ps1
@@ -28,7 +28,7 @@ Import-Module "$PSScriptRoot\Git.ps1"
 Git-Clone -Source "https://github.com/AndyD87/Powershell-Common.git" -Target "Powershell-Common"
 
 # Copy downloaded sources
-Move-Item "$PSScriptRoot\Powershell-Common\*" ".\" -Force -Exclude "UpdateCommon.ps1"
+Move-Item "$PSScriptRoot\Powershell-Common\*" ".\" -Force
 
 # Remove Folder with new sources
 Remove-Item "$PSScriptRoot\Powershell-Common" -Recurse -Force
